e671f120483729e24c9f01881a8fbc5559734594
Jeff Panici <jeff.panici@panici-software.com>
2018-06-05 04:37:53 -0600

    refactored structures
    * introduced Player and Item
    * refactored HeaderWindow and StoryWindow into InteractiveStory.

--- src/main.rs
+++ src/main.rs
@@ -3,89 +3,103 @@ extern crate textwrap;
 
 use ncurses::*;
 use textwrap::fill;
+use std::vec::Vec;
 use std::collections::HashMap;
 
-struct HeaderWindow {
-    game_title:String,
-    room_name:String,
-    score:u32,
-    moves:u32,
-    max_x:i32,
-    max_y:i32,
-    win:WINDOW
+#[derive(Copy, Clone, Hash, Eq, PartialEq, Debug)]
+enum ItemIdentifier {
+    Flashlight
 }
 
-impl HeaderWindow {
-    fn new(game_title:String) -> HeaderWindow {
-        let mut max_x = 0;
-        let mut max_y = 0;
-        getmaxyx(stdscr(), &mut max_y, &mut max_x);
-        HeaderWindow {
-            game_title,
-            room_name: String::from("The Void"),
+struct Item {
+    id: ItemIdentifier,
+    description: String,
+}
+
+impl Item {
+    fn new(id: ItemIdentifier, description: String) -> Item {
+        Item {
+            id,
+            description
+        }
+    }
+}
+
+struct Player {
+    moves: u32,
+    score: u32,
+    location: RoomIdentifier,
+    items: Vec<ItemIdentifier>,
+}
+
+impl Player {
+    fn new() -> Player {
+        Player {
             score: 0,
             moves: 0,
-            max_x,
-            max_y,
-            win: newwin(1, max_x, 0, 0)
+            items: Vec::new(),
+            location: RoomIdentifier::None,
         }
     }
 
-    fn room_name(&mut self, name:&str) {
-        self.room_name = String::from(name);
-        self.update();
-    }
-
-    fn increment_moves(&mut self) {
+    fn made_move(&mut self) {
         self.moves += 1;
     }
 
-    fn update(&mut self) {
-        let title = format!(" {} | {}", self.game_title, self.room_name);
-        let title_bar = format!(
-            "{:1$} Score: {2:06} | Moves: {3:03} ",
-            title,
-            (self.max_x as usize - (title.len() - 6)),
-            self.score,
-            self.moves);
-        leaveok(self.win, true);
-        wmove(self.win, 0, 0);
-        wattron(self.win, A_BOLD() | A_REVERSE());
-        wprintw(self.win, &title_bar);
-        wattroff(self.win, A_BOLD() | A_REVERSE());
-        wrefresh(self.win);
+    fn earned_points(&mut self, points: u32) {
+        self.score += points;
     }
 }
 
-struct StoryWindow {
-    max_x:i32,
-    max_y:i32,
-    win:WINDOW,
-    input_seq:String,
-    escape_seq:String,
-    room:RoomIdentifier,
-    header_window:HeaderWindow,
-    rooms:HashMap<RoomIdentifier, Room>,
+struct InteractiveStory {
+    max_x: i32,
+    max_y: i32,
+    player: Player,
+    input_seq: String,
+    game_title: String,
+    story_win: WINDOW,
+    header_win: WINDOW,
+    escape_seq: String,
+    items: HashMap<ItemIdentifier, Item>,
+    rooms: HashMap<RoomIdentifier, Room>,
 }
 
-impl StoryWindow {
-    fn new(header_window:HeaderWindow) -> StoryWindow {
+impl InteractiveStory {
+    fn new() -> InteractiveStory {
         let mut max_x = 0;
         let mut max_y = 0;
         getmaxyx(stdscr(), &mut max_y, &mut max_x);
-        let story_window = StoryWindow {
+        let story = InteractiveStory {
             max_x,
             max_y,
-            header_window,
+            player: Player::new(),
+            items: HashMap::new(),
             rooms: HashMap::new(),
             input_seq: String::new(),
             escape_seq: String::new(),
-            room: RoomIdentifier::None,
-            win: newwin(max_y - 1, max_x, 1, 0)
+            header_win: newwin(1, max_x, 0, 0),
+            game_title: String::from("Mystery"),
+            story_win: newwin(max_y - 1, max_x, 1, 0)
         };
-        scrollok(story_window.win, true);
-        wmove(story_window.win, 0, 0);
-        story_window
+        scrollok(story.story_win, true);
+        wmove(story.story_win, 0, 0);
+        story
+    }
+
+    fn initialize(&mut self) {
+        self.define_items();
+        self.define_rooms();
+        self.player.location = RoomIdentifier::GrimyHotelRoom;
+    }
+
+    fn define_items(&mut self) {
+        self.items.insert(
+            ItemIdentifier::Flashlight,
+            Item::new(
+                ItemIdentifier::Flashlight,
+                String::from("A sleek and modern flashlight")
+            )
+        );
     }
 
     fn define_rooms(&mut self) {
@@ -132,35 +146,31 @@ impl StoryWindow {
             ));
     }
 
-    fn room(&mut self, room_id:RoomIdentifier) {
-        self.room = room_id;
-    }
-
     fn look_at_room(&mut self) {
-        let get_room_result = self.rooms.get(&self.room);
+        self.update_header();
+
+        let get_room_result = self.rooms.get(&self.player.location);
         let room = match get_room_result {
             None => self.rooms.get(&RoomIdentifier::None).unwrap(),
             Some(room) => room
         };
-
-        self.header_window.room_name(room.name.as_str());
 
         let mut description_seq = String::new();
         description_seq.push_str(room.description.as_str());
         description_seq.push('\n');
 
         wprintw(
-            self.win,
+            self.story_win,
             &fill(description_seq.as_str(), self.max_x as usize));
     }
 
     fn prompt(&mut self) {
-        wprintw(self.win, "\n> ");
+        wprintw(self.story_win, "\n> ");
         self.input_seq.clear();
     }
 
     fn move_west(&mut self) -> RoomIdentifier {
-        let get_room_result = self.rooms.get(&self.room);
+        let get_room_result = self.rooms.get(&self.player.location);
         let room = match get_room_result {
             None => {
                 return RoomIdentifier::None;
@@ -171,7 +181,7 @@ impl StoryWindow {
     }
 
     fn move_east(&mut self) -> RoomIdentifier {
-        let get_room_result = self.rooms.get(&self.room);
+        let get_room_result = self.rooms.get(&self.player.location);
         let room = match get_room_result {
             None => {
                 return RoomIdentifier::None;
@@ -181,21 +191,42 @@ impl StoryWindow {
         room.east
     }
 
+    fn update_header(&mut self) {
+        let get_room_result = self.rooms.get(&self.player.location);
+        let room = match get_room_result {
+            None => self.rooms.get(&RoomIdentifier::None).unwrap(),
+            Some(room) => room
+        };
+
+        let title = format!(" {} | {}", self.game_title, room.name);
+        let title_bar = format!(
+            "{:1$} Score: {2:06} | Moves: {3:03} ",
+            title,
+            (self.max_x as usize - (title.len() - 6)),
+            self.player.score,
+            self.player.moves);
+        leaveok(self.header_win, true);
+        wmove(self.header_win, 0, 0);
+        wattron(self.header_win, A_BOLD() | A_REVERSE());
+        wprintw(self.header_win, &title_bar);
+        wattroff(self.header_win, A_BOLD() | A_REVERSE());
+        wrefresh(self.header_win);
+    }
+
     fn run(&mut self) {
         let mut x = 0;
         let mut y = 0;
 
-        self.header_window.update();
         self.look_at_room();
         self.prompt();
 
         loop {
-            let ch = wgetch(self.win);
-            getyx(self.win, &mut y, &mut x);
+            let ch = wgetch(self.story_win);
+            getyx(self.story_win, &mut y, &mut x);
             if ch == '\n' as i32 {
                 let mut force_look = false;
 
-                wprintw(self.win, "\n\n");
+                wprintw(self.story_win, "\n\n");
                 let result = match self.input_seq.as_ref() {
                     "quit" => {
                         break;
@@ -212,8 +243,8 @@ impl StoryWindow {
                             Some("I don't appear to be able to go in that direction.\n")
                         } else {
                             force_look = true;
-                            self.room = new_room_id;
-                            self.header_window.increment_moves();
+                            self.player.location = new_room_id;
+                            self.player.made_move();
                             Some("OK, heading west.\n")
                         }
                     }
@@ -224,8 +255,8 @@ impl StoryWindow {
                             Some("I don't appear to be able to go in that direction.\n")
                         } else {
                             force_look = true;
-                            self.room = new_room_id;
-                            self.header_window.increment_moves();
+                            self.player.location = new_room_id;
+                            self.player.made_move();
                             Some("OK, heading east.\n")
                         }
                     }
@@ -254,7 +285,7 @@ impl StoryWindow {
                 match result {
                     None => {},
                     Some(message) => {
-                        wprintw(self.win, message);
+                        wprintw(self.story_win, message);
                     }
                 };
 
@@ -265,14 +296,14 @@ impl StoryWindow {
                 self.prompt();
             } else if ch == 127 {
                 if x > 2 {
-                    mvwdelch(self.win, y, x - 1);
+                    mvwdelch(self.story_win, y, x - 1);
                 }
             } else if ch == '~' as i32 {
-                wdelch(self.win);
+                wdelch(self.story_win);
             } else {
                 if ch == 27 {
                     for _ in 0..2 {
-                        let ch = wgetch(self.win);
+                        let ch = wgetch(self.story_win);
                         self.escape_seq.push((ch as u8) as char);
                     }
                 }
@@ -280,22 +311,22 @@ impl StoryWindow {
                 if self.escape_seq.len() > 0 {
                     if self.escape_seq == "[C" {
                         if x < 64 {
-                            wmove(self.win, y, x + 1);
+                            wmove(self.story_win, y, x + 1);
                         }
                     } else if self.escape_seq == "[D" {
                         if x > 2 {
-                            wmove(self.win, y, x - 1);
+                            wmove(self.story_win, y, x - 1);
                         }
                     }
                     self.escape_seq.clear();
                 } else {
                     if x < 64 {
-                        waddch(self.win, ch as u32);
+                        waddch(self.story_win, ch as u32);
                         self.input_seq.push((ch as u8) as char);
                     }
                 }
             }
-            wrefresh(self.win);
+            wrefresh(self.story_win);
         }
     }
 }
@@ -309,15 +340,15 @@ enum RoomIdentifier {
 
 #[derive(Hash, Eq, PartialEq, Debug)]
 struct Room {
-    id:RoomIdentifier,
-    name:String,
-    description:String,
-    up:RoomIdentifier,
-    down:RoomIdentifier,
-    west:RoomIdentifier,
-    east:RoomIdentifier,
-    north:RoomIdentifier,
-    south:RoomIdentifier,
+    id: RoomIdentifier,
+    name: String,
+    description: String,
+    up: RoomIdentifier,
+    down: RoomIdentifier,
+    west: RoomIdentifier,
+    east: RoomIdentifier,
+    north: RoomIdentifier,
+    south: RoomIdentifier,
 }
 
 impl Room {
@@ -352,13 +383,9 @@ fn main() {
     keypad(stdscr(), true);
     noecho();
 
-    let header_window:HeaderWindow = HeaderWindow::new(
-        String::from("Mystery"));
-
-    let mut story_window:StoryWindow = StoryWindow::new(header_window);
-    story_window.define_rooms();
-    story_window.room(RoomIdentifier::GrimyHotelRoom);
-    story_window.run();
+    let mut story: InteractiveStory = InteractiveStory::new();
+    story.initialize();
+    story.run();
 
     endwin();
 }
